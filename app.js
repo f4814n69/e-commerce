afe5f955da77f084521b3062b83d8bc5b293f9e1
f4814n69 <fabianfigueroaN1@gmail.com>
2022-10-14 11:22:34 -0300

    e-commerce

---
+++ app.js
@@ -0,0 +1,22 @@
+const addButton = document.querySelector(".btn");
+const printImag = document.querySelector(".articles");
+const printArea = document.querySelector(".container-bus");
+
+let imagArray = [];
+
+addButton.addEventListener("click", (e) => {
+  
+  const articles = printImag.Value;
+  imagArray.push({container})
+  
+  
+  
+  console.log(imagArray);
+});
+
+/*function createImag(articles) {
+
+}*/
+
+// YA NO PUDE AVANZAR MAS :(;
+
